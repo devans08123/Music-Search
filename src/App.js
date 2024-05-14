8526979ca430c931e76fcd054d8ccb0f0891d76b
Donald Evans <devans08123@gmail.com>
2024-05-13 20:45:58 -0400

    update SRC

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
-import {useEffect, useState } from 'react'
+import { useState, Suspense, useRef } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ArtistView from './components/ArtistView'
+import AlbumView from './components/AlbumView'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import Spinner from './components/Spinner'
 import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
+import { createResource as fetchData } from 'helper'
 
-function App() {
-	let [search, setSearch] = useState('')
-	let [message, setMessage] = useState('Search for Music!')
-	let [data, setData] = useState([])
+const App = () => {
+  let searchInput = useRef('')
+  let [data, setData] = useState(null)
+  let [message, setMessage] = useState('Search for Music!')
 
-	const API_URL = 'https://itunes.apple.com/search?term='
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setData(fetchData(term, 'main'))
+  }
 
-	useEffect(() => {
-		if(search) {
-			const fetchData = async () => {
-				document.title = `${search} Music`
-				const response = await fetch(API_URL + search)
-				const resData = await response.json()
-				if (resData.results.length > 0) {
-					setData(resData.results)
-				} else {
-					setMessage('Not Found')
-				}
-			}
-			fetchData()
-		}
-	}, [search])
-	
-	const handleSearch = (e, term) => {
-		e.preventDefault()
-		setSearch(term)
-	}
+  const renderGallery = () => {
+    if(data) {
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery />
+        </Suspense>
+      )
+    }
+  }
 
-	return (
-		<div>
-			<SearchBar handleSearch = {handleSearch}/>
-			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
-		</div>
-  	);
+  return (
+    <div className="App">
+      {message}
+      <Router>
+        <Route exact path={'/'}>
+          <SearchContext.Provider value={{term: searchInput, handleSearch: handleSearch}}>
+            <SearchBar />
+          </SearchContext.Provider>
+            <DataContext.Provider value={data}>
+              {renderGallery()}
+            </DataContext.Provider>
+        </Route>
+        <Route path="/album/:id">
+          <AlbumView />
+        </Route>
+        <Route path="/artist/:id">
+          <ArtistView />
+        </Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
